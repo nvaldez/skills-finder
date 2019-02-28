88060d11051a58360fd5987473f1486ec5f762a0
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2019-02-28 11:01:51 -0500

    added landing page template and more boottrap

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { Employee, Skill } = require("../models/index");
 
 router.get("/", function(req, res) {
-  res.redirect("/employee");
+  Skill.find({}).then(skills => {
+    res.render("landing-page", { skills });
+  });
 });
 
 router.use("/employee", require("./employee"));
