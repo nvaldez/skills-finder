1119ae39c0abf41d066f56df47c2b6b851ac8c53
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2019-03-01 11:42:11 -0500

    modified views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { Employee, Skill } = require("../models/index");
 const authenticatedUser = require("../utils/authenticatedUser");
 
-router.get("/", function(req, res) {
+router.get("/", authenticatedUser, function(req, res) {
   Skill.find({}).then(skills => {
     res.render("landing-page", { skills, success: req.flash("success") });
   });
